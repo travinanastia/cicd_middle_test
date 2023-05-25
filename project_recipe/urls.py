bc2fd6a047bcb2b24859118964421cbcbabccaf7
travinanastia <nastiatravina777@gmail.com>
2023-05-25 10:37:00 +0300

    views

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('category_list/', views.category_list, name='category_list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render
+from .models import Category, Recipe
+from django.db.models import Count
+
+def category_list(request):
+    count = Category.objects.annotate(recipe_count=Count('categories'))
+    return render(request,'category_list.html',{
+        'categories':Category.objects.all(),
+        'recipes': count,
+    })
+
+def main(request):
+    return render(request,'main.html',{
+        'recipes':Recipe.objects.order_by('-id')[:5],
+    })
 
