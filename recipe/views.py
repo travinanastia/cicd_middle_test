7f96246c2806ca113d72ad3f12491798eb68e6ff
travinanastia <nastiatravina777@gmail.com>
2023-05-25 10:40:01 +0300

    com3

--- recipe/views.py
+++ recipe/views.py
@@ -3,11 +3,14 @@ from .models import Category, Recipe
 from django.db.models import Count
 
 def category_list(request):
-    count = Category.objects.annotate(recipe_count=Count('categories'))
-    return render(request,'category_list.html',{
-        'categories':Category.objects.all(),
-        'recipes': count,
-    })
+    categories = Category.objects.all()
+    category_data = []
+    for category in categories:
+        category_data.append({
+            'name': category.name,
+            'recipe_count': Recipe.objects.filter(category=category).count()
+        })
+    return render(request, 'category_list.html', {'categories': category_data})
 
 def main(request):
     return render(request,'main.html',{
